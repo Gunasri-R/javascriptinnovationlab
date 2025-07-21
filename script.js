aa6250039f0eb28372f345ba14549d6d8a6316aa
Gunasri_Rameshkumar <gunasriramesh07@gmail.com>
2025-07-21 15:35:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log()
+console.log()
+function addition(){
+    var num1 = parseFloat(document.getElementById("input1").value)
+    var num2 = parseFloat(document.getElementById("input2").value)
+    console.log(num1+num2)
+}
+function multiplication(){
+    var num1 = (document.getElementById("input1").value)
+    var num2 = (document.getElementById("input2").value)
+    console.log(num1*num2)
+    
+}
+function subtraction(){
+    var num1 = (document.getElementById("input1").value)
+    var num2 = (document.getElementById("input2").value)
+    console.log(num1-num2)
+}
+function division(){
+    var num1 = (document.getElementById("input1").value)
+    var num2 = (document.getElementById("input2").value)
+    console.log(num1/num2)
+}
+    
